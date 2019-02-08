a4878d7e1e69a150479c001c1ebb5b4c1a142bdf
XinyuJiang <503273921@qq.com>
2019-02-09 00:32:54 +0800

    模型创建

---
+++ psybot/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class PsybotConfig(AppConfig):
+    name = 'psybot'
---
+++ psybot/models.py
@@ -0,0 +1,76 @@
+from django.db import models
+
+
+# Create your models here.
+class Userinfo(models.Model):
+    nickname = models.CharField(max_length=50)
+    password = models.CharField(max_length=20)
+    touxiang = models.CharField(max_length=200)
+    sex = models.CharField(max_length=10, null=True, blank=True)#数据库内可以为空值，并且Django角度插入内容可以是空
+    age = models.IntegerField(null=True, blank=True)
+    department = models.CharField(max_length=50,null=True,blank=True)
+    email = models.CharField(max_length=50)
+
+
+# 存储用户打开程序时间，关闭程序时间
+class Activityinfo(models.Model):
+    user = models.ForeignKey(Userinfo, on_delete=models.CASCADE)
+    open_time = models.DateField(auto_now_add=True)
+    end_time = models.DateField(auto_now_add=True)
+
+
+class Speechinfo(models.Model):
+    user = models.ForeignKey(Userinfo, on_delete=models.CASCADE)
+    text = models.CharField(max_length=1000)
+    create_time = models.DateField(auto_now_add=True)
+    label = models.IntegerField(null=True, blank=True)
+
+
+class Emotioninfo(models.Model):
+    user = models.ForeignKey(Userinfo, on_delete=models.CASCADE)
+    awake = models.IntegerField(null=True, blank=True)
+    content = models.CharField(max_length=300)
+    create_time = models.DateField(auto_now_add=True)
+    efficient = models.CharField(max_length=30)
+    belief = models.CharField(max_length=50)
+
+
+class Psychologist (models.Model):
+    nickname = models.CharField(max_length=50)
+    password = models.CharField(max_length=20)
+    touxiang = models.CharField(max_length=200)
+    sex = models.CharField(max_length=10, null=True, blank=True)  # 数据库内可以为空值，并且Django角度插入内容可以是空
+    age = models.IntegerField(null=True, blank=True)
+    department = models.CharField(max_length=50, null=True, blank=True)
+    email = models.CharField(max_length=50)
+    create_time = models.DateField(auto_now_add=True)
+
+
+class Course_situation(models.Model):
+    user = models.ForeignKey(Userinfo, on_delete=models.CASCADE)
+    text = models.CharField(max_length=1000)
+    image = models.CharField(max_length=1000)
+
+
+class Report_situation(models.Model):
+    user = models.ForeignKey(Userinfo, on_delete=models.CASCADE)
+    text = models.CharField(max_length=1000)
+    create_time = models.DateField(auto_now_add=True)
+
+
+class Init_situation(models.Model):
+    user = models.ForeignKey(Userinfo, on_delete=models.CASCADE)
+    text = models.CharField(max_length=1000)
+    image = models.CharField(max_length=1000)
+
+
+class Relax_situation(models.Model):
+    user = models.ForeignKey(Userinfo, on_delete=models.CASCADE)
+    practice_type = models.CharField(max_length=50)
+    text = models.CharField(max_length=1000)
+    image = models.CharField(max_length=1000)
+
+
+class Check_situation(models.Model):
+    user = models.ForeignKey(Userinfo, on_delete=models.CASCADE)
+    create_time = models.DateField(auto_now_add=True)
